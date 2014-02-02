559fae8a43f644eb232ede18e7bb38827b6bdf5a
jongulrich <jongulrich@gmail.com>
2014-02-02 16:21:25 -0500

    Added some functions to read/write
    I should probably test them, I need to at least test logic, but this is a step in the right direction

--- Pidar/dynamixel.cpp
+++ Pidar/dynamixel.cpp
@@ -8,6 +8,8 @@
 /// Created: 1/28/13
 /// Email: jongulrich@gmail.com, watsontandrew@gmail.com
 ///
+/// Rx-24f api address reference:
+/// http://support.robotis.com/en/product/dynamixel/rx_series/rx-24f.htm
 ////////////////////////////////////////////////////////////////////////////////
 #include "dynamixel.hpp"
 
@@ -104,20 +106,36 @@ void Dynamixel::StopCaptureThread()
     using RAM address 30 & 31 */
 void Dynamixel::SetPosition(const double val)
 {
+    //16 bit address (8 + 8)
+    //30 (0X1E)Goal Position(L) Lowest byte of Goal Position RW
+    //31 (0X1F)Goal Position(H) Highest byte of Goal Position RW
 
+    //dxl_write_word((int id, int address, int value)
+    int8_t firstbyte = (int)val & 0xFF;
+    int8_t secondbyte = (int)val >> 8;
+
+    dxl_write_word(mID, 0x1E, firstbyte ); //low
+    dxl_write_word(mID, 0x1F, secondbyte ); //high
 }
 
 
 /** Set speed of motor speed (RPM) using RAM address 32 & 33 */
 void Dynamixel::SetSpeed(const double rpm)
 {
-
+    int8_t firstbyte = (int)rpm & 0xFF;
+    int8_t secondbyte = (int)rpm >> 8;
+    dxl_write_word(mID, 0x20, firstbyte ); //low
+    dxl_write_word(mID, 0x21, secondbyte ); //high
 }
 
 
 /** Set torque of motor (0 to 1023) using RAM address 34 & 35 */
 void Dynamixel::SetTorque(const int val)
 {
+    int8_t firstbyte = (int)val & 0xFF;
+    int8_t secondbyte = (int)val >> 8;
+    dxl_write_word(mID, 0x22, firstbyte ); //low
+    dxl_write_word(mID, 0x23, secondbyte ); //high
 
 }
 
@@ -126,7 +144,13 @@ void Dynamixel::SetTorque(const int val)
     using RAM address 36 & 37 */
 double Dynamixel::GetPresentPosition()
 {
-    return 0.0;
+    double val = 0.0;
+    int8_t firstbyte = dxl_read_word( mID, 0x24 );
+    int8_t secondbyte = dxl_read_word( mID, 0x25 );
+
+    val = (secondbyte << 8)  | firstbyte;
+
+    return val;
 }
 
 
